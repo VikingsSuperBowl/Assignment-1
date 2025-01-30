24a669b98f04d96209d8f056fa0673a7604d781c
VikingsSuperBowl <101305886@georgebrown.ca>
2025-01-30 21:45:45 +0000

    it works pt 4

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 function start() {
 
     // Bar Chart
-
     const ctx = document.getElementById('myBarChart');
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Revenues', 'Expenses'],
+            labels: ['QB Pressure Rate', 'Ball Security', 'INT Opportunities', 'Strip Sack Rate', 'Turnover Margin'],
             datasets: [{
-                label: 'In millions',
-                data: [444, 421],
-                borderWidth: 1
+                label: 'Eagles',
+                data: [35, 92, 12, 3.2, 65],
+                borderWidth: 1,
+                backgroundColor: 'rgba(0, 76, 84, 0.6)', // Eagles color
+                borderColor: 'rgba(0, 76, 84, 1)'
+            },
+            {
+                label: 'Chiefs',
+                data: [26, 85, 8, 2.1, 55],
+                borderWidth: 1,
+                backgroundColor: 'rgba(202, 36, 48, 0.6)', // Chiefs color
+                borderColor: 'rgba(202, 36, 48, 1)'
             }]
         },
         options: {
@@ -23,14 +31,13 @@ function start() {
     });
 
     // Pie Chart
-
     const pieCtx = document.getElementById('myPieChart');
     new Chart(pieCtx, {
         type: 'pie',
         data: {
             labels: ['Eagles Win','Chiefs Win'],
             datasets: [{
-                label: '# of responses',
+                label: '% of fans',
                 data: [57,43],
                 borderWidth: 1,
                 backgroundColor: ["#004C54","#CA2430"],
@@ -38,19 +45,33 @@ function start() {
         }
     });
 
-
     // Line Chart
-
     const lineCtx = document.getElementById('lineChart');
     new Chart(lineCtx, {
         type: 'line',
         data: {
-            labels: ['2008', '2009', '2010', '2011'],
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [{
-                label: 'George Brown College',
-                data: [12748, 13866, 13914, 14068],
-                borderWidth: 1
+                label: 'Eagles',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                borderColor: 'rgba(0, 76, 84, 1)',
+                backgroundColor: 'rgba(0, 76, 84, 0.2)',
+                fill: false
+            },
+            {
+                label: 'Chiefs',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                borderColor: 'rgba(202, 36, 48, 1)',
+                backgroundColor: 'rgba(202, 36, 48, 0.2)',
+                fill: false
             }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
     });
 
